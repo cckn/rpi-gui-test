754ad8ed3053cbce3aab48b5ea080edcf0089246
cckn <cckn.dev@gmail.com>
2021-01-11 00:09:46 +0900

    그리드 구조로 변경

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "./app.css";
 import axios from "axios";
+import Chart from "chart.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const $now: HTMLSpanElement | null = document.querySelector(".now");
@@ -39,6 +40,21 @@ document.addEventListener("DOMContentLoaded", () => {
     if ($controlSpan) $controlSpan.innerText = data;
   });
 
+  const $chart: HTMLCanvasElement | null = document.querySelector("#myChart");
+
+  let chart: Chart;
+  const data: number[] = [];
+  const labels: number[] = [];
+  let count = 0;
+  const ctx = $chart?.getContext("2d");
+
+  if (ctx) {
+    chart = new Chart(ctx, {
+      type: "line",
+      data: {},
+    });
+  }
+
   const updata = async () => {
     const now = new Date();
 
@@ -47,9 +63,56 @@ document.addEventListener("DOMContentLoaded", () => {
     );
     const { data: state } = await axios.get("http://localhost:3000/api/state");
 
-    if ($now) $now.innerHTML = `${now.toLocaleString()}`;
-    if ($randomSpan) $randomSpan.innerText = random;
+    data.push(parseFloat(random));
+    labels.push(count++);
+
+    if (data.length > 100) {
+      data.shift();
+      labels.shift();
+    }
+
+    if ($now) $now.innerHTML = `${now.toLocaleTimeString()}`;
+    if ($randomSpan) $randomSpan.innerText = random.toFixed(2);
     if ($controlSpan) $controlSpan.innerText = state;
+
+    if ($chart && ctx) {
+      if (chart) {
+        chart.destroy();
+      }
+      if (ctx && $chart) {
+        chart = new Chart(ctx, {
+          type: "line",
+          data: {
+            labels,
+            datasets: [
+              {
+                label: "테스트 데이터셋",
+                data,
+                borderColor: "skyblue",
+                backgroundColor: "skyblue",
+                fill: true,
+              },
+            ],
+          },
+
+          options: {
+            animation: {
+              duration: 0,
+            },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    min: Math.min(...data) - 100,
+                    max: Math.max(...data) + 100,
+                  },
+                },
+              ],
+            },
+          },
+        });
+      }
+    }
   };
 
   updata();
